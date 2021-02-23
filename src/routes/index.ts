e5d71f5f2986506e0ce70f64287ed7ee8a0e8032
Luis Carlos <roxisxprol@gmail.com>
2021-02-23 15:40:13 -0300

    feat: Day - 2

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,8 @@
 import { Router, RouterOptions } from 'express';
+import usersRouter from './user.routes'
 
 const routes = Router();
+routes.use('/users', usersRouter)
 
 routes.get('/', (request, response) => {
   return response.json({msg: 'NLW4 - Trilha Node'})
---
+++ src/routes/user.routes.ts
@@ -0,0 +1,9 @@
+import { Router } from 'express'
+import {UserController} from '../controllers/UserController'
+
+const userController = new UserController()
+const userRouter = Router()
+
+userRouter.post('/', userController.create)
+
+export default userRouter
